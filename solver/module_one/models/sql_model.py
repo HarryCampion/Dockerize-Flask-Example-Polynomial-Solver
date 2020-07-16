93381cdf0d3a3b1044f21cc34c9e201fae082ffd
Harry Campion <harry@thecampions.co.uk>
2020-07-16 12:32:44 +0100

    sql mock hide attributes

--- solver/module_one/models/sql_model.py
+++ solver/module_one/models/sql_model.py
@@ -6,12 +6,9 @@ class SqlMock():
     def __init__(self, connection=":memory:"):
         """
         SQL db stored in memory. Cached on start.
-        Atttributes:
-            x: list 
-            y: list
         """
-        self.conn = sqlite3.connect(connection)
-        self.cur = self.conn.cursor()
+        self.__conn = sqlite3.connect(connection)
+        self.__cur = self.conn.cursor()
     
     def create_table(self, table_name:str, columns: list):
         """
@@ -20,7 +17,7 @@ class SqlMock():
         table_name: "something"
         columns: ["x", "y"]
         """
-        self.cur.execute("""CREATE TABLE {}
+        self.__cur.execute("""CREATE TABLE {}
              ({})""".format(table_name, ",".join(columns)))
 
     def insert_rows(self, table_name: str, columns: list, values: list):
@@ -36,14 +33,14 @@ class SqlMock():
                     VALUES ({});""".format(table_name, ",".join(columns), ",".join(["?" for column in columns])))
 
         records = list(zip(*values))
-        self.cur.executemany(insert_query, records)
+        self.cur.__executemany(insert_query, records)
 
     def read_table(self, table_name: str):
         """
         Read all data from table
         """
-        self.cur.execute("""SELECT * FROM {}""".format(table_name))
-        return self.cur.fetchall()
+        self.__cur.execute("""SELECT * FROM {}""".format(table_name))
+        return self.__cur.fetchall()
     
     def run_mock(self, table_name: str, columns: list, values: list):
         """
@@ -51,10 +48,10 @@ class SqlMock():
         """
         self.create_table(table_name, columns)
         self.insert_rows(table_name, columns, values)
-        self.conn.commit()
+        self.__conn.commit()
 
     def close_conn(self):
         """
         Close the connection.
         """
-        self.conn.close()
+        self.__conn.close()
